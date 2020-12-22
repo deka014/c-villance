afb1ae40ad1c63eff940feef72594fd8eaaa0c88
Deepjyoti Deka <deepjyotideka8@gmail.com>
2020-12-23 03:00:56 +0530

    installed material ui and completed country code import in dropdowm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
+import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+
+  const getCountriesData = async () => {
+    const response = await fetch("https://disease.sh/v3/covid-19/countries");
+    const data = await response.json();
+    const countries = data.map((country) => {
+      return { name: country.country, value: country.countryInfo.iso2 };
+    });
+
+    setCountries(countries);
+  };
+
+  const onCountryChange = async (event) => {
+    setCountry(event.target.value);
+  };
+
+  useEffect(() => {
+    // async function
+    getCountriesData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>;
+            {countries.map((country) => {
+              return <MenuItem value={country.value}>{country.name}</MenuItem>;
+            })}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
